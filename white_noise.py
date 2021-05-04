503b1882d8f2ab9143dab6d0a03640f55a7a584a
francescacolacrai <64028257+francescacolacrai@users.noreply.github.com>
2021-05-04 21:49:13 +0200

    Add files via upload

--- white_noise.py
+++ white_noise.py
@@ -1,8 +1,12 @@
-import random as rd
+#GENERATION OF A GAUSSIAN WHITE NOISE (~WN[O,lambda^2])
+
 import pandas as pd
 import pandas.plotting as pdpl
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np 
+import statistics as st
+from statsmodels.graphics import tsaplots
 
 #define plot style
 plt.style.use(['classic', '_classic_test_patch'])
@@ -10,39 +14,48 @@ mpl.rcParams['lines.linewidth'] = 0.5
 mpl.rcParams['lines.linestyle'] = '-'
 
 #seed random number generator
-rd.seed(1)
+np.random.seed(1)
 
-#create white noise series
-series = [rd.gauss(0.0, 1.0) for i in range(10000)]
-series = pd.Series(series)
+#create white noise 
+media = 0 
+dev_st = 1
+varianza = dev_st ^ 2
+gauss = media + dev_st * np.random.randn(10000)
 
-#summary stats
-print(series.describe())
+#verify properties
+print('\n La media è: \n', st.mean(gauss))
+print('\n La deviazione standard è: \n', st.stdev(gauss))
+print('\n La varianza è: \n', st.variance(gauss), '\n')
 
 #line plot
-plt.plot(series, color='navy', label='White Noise Series')
-plt.title('White Noise($\\mu=0$, $\\sigma^2$=1)')
+plt.plot(gauss, color='navy', label='White Noise Series')
+plt.title('White Noise($ \\mu=0$, $\\sigma^2$=1)', fontweight='bold')
 plt.xlabel('Cross-section')
 plt.ylabel("Values")
 plt.axis([0, 10000, -4.5, 5.0])
-plt.legend(frameon=True, shadow=True, borderpad=1)
 plt.grid(True)
 plt.show()
 
 #histogram plot 
-plt.hist(series, color='navy', edgecolor='k', label='Histogram')
-plt.title('Probability distribution of white noise(gaussian) ')
+num_bins = 100
+n, bins, patches = plt.hist(gauss, num_bins, density = 3, color='#006400', edgecolor='k', label='Histogram')  
+PDF_Gauss = (1/(np.sqrt(2*np.pi)*dev_st)) * np.exp(-((bins-media)**2)/(2*dev_st**2))
+plt.plot(bins, PDF_Gauss, color='#FF4500', linewidth=2, label='Gaussian PDF')
+plt.text(-3.7,0.3,r'$p_X(x)=\frac{1}{\lambda \sqrt{2\pi}}e^{-\frac{(x-\mu)^2}{2\lambda^2}} \searrow$', size=14)
+plt.title('Probability distribution of white noise (gaussian)', fontweight='bold')
 plt.xlabel('')
 plt.ylabel('Distribution')
 plt.legend(frameon=True, shadow=True, borderpad=1)
-plt.grid(True)
 plt.show()
 
 #autocorrelation plot
-pdpl.autocorrelation_plot(series, color='navy', label='Autocorrelation')
-plt.title('Autocorrelation plot')
+acorr = pdpl.autocorrelation_plot(gauss, color='#800000', lw=1, label='Autocorrelation')
+acorr.set_xlim([0,500])
+plt.title('Autocorrelation plot', fontweight='bold')
 plt.xlabel('Lag')
 plt.ylabel('Autocorrelation')
-plt.axis([0,10000,-0.3,0.3])
+plt.axis([0,500,-0.3,0.3])
 plt.legend(frameon=True, shadow=True, borderpad=1)
 plt.show()
+
+
