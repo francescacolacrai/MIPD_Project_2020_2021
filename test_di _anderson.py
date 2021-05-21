e12d6748e9808c9f45e60fa1b41b989cfa714040
francescacolacrai <64028257+francescacolacrai@users.noreply.github.com>
2021-05-21 18:47:50 +0200

    Test di Anderson

--- test_di _anderson.py
+++ test_di _anderson.py
@@ -3,14 +3,15 @@
 #Importiamo inizialmente le librerie necessarie
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm as nm
 
 #Importiamo i dati del file 
 #from generazione_rumore import gauss
 
 #generazione segnale per provare la funzione 'Anderson'
 np.random.seed(1)
-media = 3
-var = 4
+media = 2
+var = 1
 dev = np.sqrt(var)
 signal = media + dev * np.random.randn(10000)
 
@@ -27,7 +28,8 @@ alpha = 0.05 # 5%
  #Determino il parametro beta: percentile è una misura utilizzata in statistica 
 #che indica il valore in corrispondenza del quale cade una data percentuale di osservazioni
 #Forniamo '2.5' perché consideriamo la singola coda (l'area complessiva coperta dalle due code è 5)
-beta = abs(np.quantile(signal, alpha/2))
+#beta = abs(np.quantile(gauss, alpha/2))
+beta = abs(nm.ppf(alpha/2))
 #beta = abs(np.percentile(signal, (alpha*100)/2))
 print ('Il parametro beta vale: \n', round(beta,6))
 
@@ -42,17 +44,30 @@ def Anderson(rumore,alpha,tau_max) :
         gamma = np.zeros((tau_max+1),dtype=int)
         segnale_t = segnale.transpose()
         #gamma_zero: stima della varianza del segnale 'gauss'
-        gamma_zero = np.dot(segnale[1:tau_max], segnale_t[1:tau_max])
+        gamma_zero = (1/tau_max) * np.dot(segnale[1:tau_max], segnale_t[1:tau_max])
         for t in range(0,tau_max):
-            gamma[t+1] = np.dot(segnale[1:tau_max-t], segnale_t[1+t:tau_max])
+            gamma[t+1] = (1/tau_max) * np.dot(segnale[1:tau_max-t], segnale_t[1+t:tau_max])
             rho = gamma / gamma_zero
         return rho
-        
+
     print('\nRho vale: \n', Rho(rumore,tau_max))
 
     #estremo dell'intervallo di confidenza
     estremo = beta/np.sqrt(tau_max)
     print('\nGli estremi di intervallo di confidenza sono: \n', -round(estremo,6), ' e ', round(estremo,6), '\n')
+
+
+    #grafico della covarianza appena ottenuta
+    plt.figure('Stima della covarianza campionaria normalizzata')
+    for i in range (tau_max+1) :
+        #plt.plot(i, Rho(rumore,tau_max), linewidth=1.5)
+        plt.plot(range(0,tau_max+1),Rho(rumore,tau_max))
+    sup = plt.axhline(estremo, xmin=0, xmax=tau_max)
+    inf = plt.axhline(-estremo, xmin=0, xmax=tau_max)
+    plt.title('Grafico di una stima di covarianza campionaria normalizzata \n' r'$\mathbf{\hat\rho(\tau)}$', fontweight='bold')
+    plt.xlabel('Ritardo ' r'$\tau$')
+    plt.ylabel(r'$\rho(\tau)$')
+    plt.show()
 
     #Calcolo dei valori che cadono al di fuori dell'intervallo di confidenza dato
     def Conteggio(segnale,tau_max) :
@@ -75,17 +90,5 @@ def Anderson(rumore,alpha,tau_max) :
 
 #Applico la funzione al segnale 'gauss' generato in precedenza
 test = Anderson(signal,alpha,tau_max)
-
-#grafico della covarianza appena ottenuta
-fig = plt.figure('Stima della covarianza campionaria normalizzata')
-#for i in range (tau_max+1) :
-    #plt.plot(i, rho, linewidth=1.5)
-#plt.plot(range(0,tau_max+1),rho)
-#sup = plt.axhline(estremo, xmin=0, xmax=tau_max)
-#inf = plt.axhline(-estremo, xmin=0, xmax=tau_max)
-plt.title('Grafico di una stima di covarianza campionaria normalizzata \n' r'$\mathbf{\hat\rho(\tau)}$', fontweight='bold')
-plt.xlabel('Ritardo ' r'$\tau$')
-plt.ylabel(r'$\rho(\tau)$')
-plt.show()
 
 print('\n Il risultato del test è: \n', test, '\n')
