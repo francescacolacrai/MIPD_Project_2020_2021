b38b3ae63a3214ee0d4b7ea44700cc46d7dbedd5
francescacolacrai <64028257+francescacolacrai@users.noreply.github.com>
2021-05-24 11:23:21 +0200

    test di Anderson

--- test_di _anderson.py
+++ test_di _anderson.py
@@ -6,74 +6,89 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm as nm
 
 #Importiamo i dati del file 
-#from generazione_rumore import gauss
-
+from segnale import gauss
+    
 #generazione segnale per provare la funzione 'Anderson'
-np.random.seed(1)
-media = 2
-var = 1
-dev = np.sqrt(var)
-signal = media + dev * np.random.randn(10000)
+#np.random.seed(1)
+#media = 2
+#var = 2
+#dev = np.sqrt(var)
+#signal = media + dev * np.random.randn(10000)
 
 #Numero di campioni
-N = signal.size 
-signal_t = signal.transpose()
-
-#Scelgo il ritardo massimo
-tau_max = 30
+#N = gauss.size 
+gauss_t = gauss.transpose()
 
 #Scelgo il valore del livello di confidenza alpha
 alpha = 0.05 # 5%
 
- #Determino il parametro beta: percentile è una misura utilizzata in statistica 
-#che indica il valore in corrispondenza del quale cade una data percentuale di osservazioni
-#Forniamo '2.5' perché consideriamo la singola coda (l'area complessiva coperta dalle due code è 5)
-#beta = abs(np.quantile(gauss, alpha/2))
-beta = abs(nm.ppf(alpha/2))
-#beta = abs(np.percentile(signal, (alpha*100)/2))
-print ('Il parametro beta vale: \n', round(beta,6))
+#Scelgo il ritardo massimo
+tau_max = 30
 
-#Controllo livello di significatività alpha
-if alpha <= 0 or alpha >= 1 :
-    raise ValueError('***Il livello di significatività alpha deve stare in (0,1)***')
+def Anderson(rumore,tau_max=30,alpha=0.05) :
 
-def Anderson(rumore,alpha,tau_max) :
+    #Controllo del passaggio del parametro tau_max
+    if tau_max >= len(gauss) or tau_max <0:
+        print('\n')
+        raise ValueError('*** tau_max deve essere positivo ed minore della lungezza del segnale ***')
+        
+    #Determino il parametro beta: 
+    #forniamo 'alpha/2' perché consideriamo la singola coda (l'area complessiva delle due code è 5)
+    #beta = abs(np.quantile(gauss, alpha/2))
+    beta = abs(nm.ppf(alpha/2))
+    print ('Il parametro beta vale: \n', round(beta,6)) 
+
+    #Controllo livello di significatività alpha
+    if alpha <= 0 or alpha >= 1 :
+        print('\n')
+        raise ValueError('*** Il livello di significatività alpha deve stare in (0,1) ***')
 
     #Calcolo una stima della funzione di covarianza campionaria normalizzata
     def Rho(segnale,tau_max) :
         gamma = np.zeros((tau_max+1),dtype=int)
         segnale_t = segnale.transpose()
-        #gamma_zero: stima della varianza del segnale 'gauss'
-        gamma_zero = (1/tau_max) * np.dot(segnale[1:tau_max], segnale_t[1:tau_max])
+        #gamma_zero = (1/tau_max) * np.dot(segnale[0:tau_max], segnale_t[0:tau_max])
         for t in range(0,tau_max):
-            gamma[t+1] = (1/tau_max) * np.dot(segnale[1:tau_max-t], segnale_t[1+t:tau_max])
-            rho = gamma / gamma_zero
+            gamma[t] = (1/tau_max) * np.dot(segnale[0:tau_max-t], segnale_t[t:tau_max])
+        rho = gamma / gamma[0]
         return rho
 
     print('\nRho vale: \n', Rho(rumore,tau_max))
 
     #estremo dell'intervallo di confidenza
     estremo = beta/np.sqrt(tau_max)
-    print('\nGli estremi di intervallo di confidenza sono: \n', -round(estremo,6), ' e ', round(estremo,6), '\n')
-
+    print('\nGli estremi di intervallo di confidenza sono: \n', -round(estremo,6), ' e ', \
+            round(estremo,6), '\n')
 
     #grafico della covarianza appena ottenuta
     plt.figure('Stima della covarianza campionaria normalizzata')
     #for i in range (tau_max+1) :
         #plt.plot(i, Rho(rumore,tau_max), linewidth=1.5)
-    plt.plot(range(0,tau_max+1),Rho(rumore,tau_max))
-    sup = plt.axhline(estremo, xmin=0, xmax=tau_max)
-    inf = plt.axhline(-estremo, xmin=0, xmax=tau_max)
-    plt.title('Grafico di una stima di covarianza campionaria normalizzata \n' r'$\mathbf{\hat\rho(\tau)}$', fontweight='bold')
+    plt.plot(range(0,tau_max+1), Rho(rumore,tau_max), color='darkcyan', \
+             label='Stima della covarianza campionaria ' r'$\hat\rho(\tau)$', linewidth=1.5)
+    #Estremo superiore dell'intervallo di confidenza
+    plt.axhline(estremo, xmin=0, xmax=tau_max, color='orange', \
+                      label='Estremo superiore di ' \
+                      r'$(-\frac{\beta}{\sqrt{\tau_{max}}},\frac{\beta}{\sqrt{\tau_{max}}})$', 
+                      linewidth=1.5)
+    #Estremo inferiore dell'intervallo di confidenza
+    plt.axhline(-estremo, xmin=0, xmax=tau_max, color='red', label='Estremo inferiore di ' \
+                      r'$(-\frac{\beta}{\sqrt{\tau_{max}}},\frac{\beta}{\sqrt{\tau_{max}}})$', \
+                      linewidth=1.5)
+    plt.axis([0,30,-0.4,1])
+    plt.title('Grafico di una stima di covarianza campionaria normalizzata \n' \
+               r'$\mathbf{\hat\rho(\tau)}$', fontweight='bold')
     plt.xlabel('Ritardo ' r'$\tau$')
-    plt.ylabel(r'$\rho(\tau)$')
+    plt.ylabel(r'$\hat\rho(\tau)$')
+    plt.legend(frameon=True, shadow=True, borderpad=1)
     plt.show()
 
     #Calcolo dei valori che cadono al di fuori dell'intervallo di confidenza dato
     def Conteggio(segnale,tau_max) :
         fuori = 0
         for i in range (1,len(Rho(segnale,tau_max))) :
-            if np.any(Rho(segnale,tau_max)[i] > beta / np.sqrt(tau_max)) or np.any(Rho(segnale,tau_max)[i] < -beta / np.sqrt(tau_max)) :
+            if np.any(Rho(segnale,tau_max)[i] > beta / np.sqrt(tau_max)) or \
+               np.any(Rho(segnale,tau_max)[i] < -beta / np.sqrt(tau_max)) :
                 fuori += 1
         return fuori
 
@@ -89,6 +104,6 @@ def Anderson(rumore,alpha,tau_max) :
     return [Test(rumore,tau_max,alpha)]
 
 #Applico la funzione al segnale 'gauss' generato in precedenza
-test = Anderson(signal,alpha,tau_max)
+test = Anderson(gauss,tau_max,alpha)
 
 print('\n Il risultato del test è: \n', test, '\n')
