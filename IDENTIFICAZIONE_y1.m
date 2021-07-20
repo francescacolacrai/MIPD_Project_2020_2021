da2e132fc7e7b0a9807d795b9a64bd90e6e99fc9
francescacolacrai <64028257+francescacolacrai@users.noreply.github.com>
2021-07-20 18:28:40 +0200

    identificazione arx su y1

--- IDENTIFICAZIONE_y1.m
+++ IDENTIFICAZIONE_y1.m
@@ -1,6 +1,6 @@
 %IDENTIFICAZIONE%
 
-%carico i le coppie ingresso uscita osservate
+%carico i le coppie ingresso-uscita osservate
 load('ES_4.MAT');
 
 %divido a metà i dati: la prima parte per identificazione e la seconda
@@ -11,6 +11,10 @@ y1_id = es4_y1(1:150);
 y1_val = es4_y1(151:301);
 dati_id = [y1_id u_id];
 dati_val = [y1_val u_val];
+
+%elimino la media dai dati
+dati_id_detrend = detrend(dati_id);
+dati_val_detrend = detrend(dati_val);
 
 %creo modelli nell'intorno dell'ordine trovato
 n = input("Inserisci l'ordine ottenuto: ");
@@ -26,30 +30,48 @@ switch scelta
         n_model = size(modello);
 
         % so che l'ARX è dato da y(t) = [B(z)/A(z)]*u(t-1) + [1/A(z)]*xi(t) e 
-        % A(z)/B(z) deve essere strettamente proprio
+        % B(z)/A(z) deve essere strettamente propria
         idx = find(modello(:,2) > modello(:,1));
         modello(idx,:) = [];
         n_model = size(modello);
-        fprintf("I modelli nell'intorno dell'ordine appena trovato sono: \n");
+        fprintf("\nI modelli nell'intorno dell'ordine appena trovato sono: \n");
         disp(modello);
         
-        for i = 1:n_model
-            theta = arx(dati_id,[modello(i,1),modello(i,2),modello(i,3)]); %creo il modello
-            fprintf("Modello ARX(");
+        Incertezza_max = zeros(n_model(1,1),1); %inizializzo il vettore delle incertezze massime
+        for i = 1:n_model(1,1)
+            Nome(i) = "arx(" + modello(i,1) + ',' + modello(i,2) + ',' + modello(i,3) + ')';
+            fprintf("\nModello ARX(");
             fprintf("%d",modello(i,1));
             fprintf(",")
             fprintf("%d",modello(i,2));
             fprintf(",")
             fprintf("%d",modello(i,3));
-            fprintf(") \n\n");
-            Errore_id_arx(theta,dati_id,modello(i,1),modello(i,2),modello(i,3));
-            inc_max = Incertezza_Coefficienti(theta);
-            v_inc_max(i) = inc_max;
+            fprintf(")\n");
+            theta = arx(dati_id_detrend,[modello(i,1),modello(i,2),modello(i,3)]) %creo il modello
+            Errore_id_arx(theta,dati_id_detrend,modello(i,1),modello(i,2),modello(i,3)); %test di anderson
+            inc_max = Incertezza_Coefficienti(theta); %valuto l'incertezza massima
+            Incertezza_max(i) = inc_max;
         end
-        %ordino in modo crescente i modelli rispetto all'incertezza massima
-        v_inc_max = v_inc_max';
-        fprintf("\nLe incertezze massime in ordine crescente sono: \n");
-        v_inc_max = sortrows(v_inc_max,1)
+    
+        Nome = Nome';
+        titolo = ["Modello", "Incertezza massima"];
+        fprintf("I modelli con le relative incertezze sono: \n");
+        Esito = [titolo; Nome Incertezza_max] 
+     
+        %elimino i modelli con le incertezze elevate
+        l = size(Esito);
+        incertezze = zeros(l(1,1),1);
+        for i = 2 : l(1,1)
+            incertezze(i) = str2num(Esito(i,2));
+        end
+        ind = find(incertezze(:) > 100);
+        incertezze(ind) = [];
+        Esito(ind,:) = [];
+        incertezze(1) = [];
+        fprintf("Le incertezze apprezzabili sono: \n");
+        incertezze
+        fprintf("I modelli con le incertezze accettabili sono: \n");
+        Esito
     case 'armax'
         modello = struc(n-1:n,n-1:n+1,n-1:n+1,1:2);
         n_model = size(modello);
@@ -62,8 +84,8 @@ switch scelta
         fprintf("I modelli nell'intorno dell'ordine appena trovato sono: \n");
         disp(modello);
 
+        Incertezza_max = zeros(n_model(1,1),1); %inizializzo il vettore delle incertezze massime
         for i = 1:n_model
-            theta = armax(dati_id,[modello(i,1),modello(i,2),modello(i,3),modello(i,4)]); %creo il modello
             fprintf("Modello ARMAX(");
             fprintf("%d",modello(i,1));
             fprintf(',')
@@ -72,54 +94,33 @@ switch scelta
             fprintf("%d",modello(i,3));
             fprintf(',')
             fprintf("%d",modello(i,4));
-            fprintf(") \n\n");
-            Errore_id_armax(theta,dati_id,modello(i,1),modello(i,2),modello(i,3),modello(i,4));
+            fprintf(")\n");
+            theta = armax(dati_id_detrend,[modello(i,1),modello(i,2),modello(i,3),modello(i,4)]); %creo il modello
+            Errore_id_armax(theta,dati_id_detrend,modello(i,1),modello(i,2),modello(i,3),modello(i,4));
             inc_max = Incertezza_Coefficienti(theta);
-            v_inc_max(i) = inc_max;
+            Incertezza_max(i) = inc_max;
         end
         
-        %ordino in modo crescente i modelli rispetto all'incertezza massima
-        v_inc_max = v_inc_max';
-        fprintf("\nLe incertezze massime in ordine crescente sono: \n");
-        v_inc_max = sortrows(v_inc_max,1);
+        Nome = Nome';
+        titolo = ["Modello", "Incertezza massima"];
+        fprintf("I modelli con le relative incertezze sono: \n");
+        Esito = [titolo; Nome Incertezza_max] 
+     
+        %elimino i modelli con le incertezze elevate
+        l = size(Esito);
+        incertezze = zeros(l(1,1),1);
+        for i = 2 : l(1,1)
+            incertezze(i) = str2num(Esito(i,2));
+        end
+        ind = find(incertezze(:) > 100);
+        incertezze(ind) = [];
+        Esito(ind,:) = [];
+        incertezze(1) = [];
+        fprintf("Le incertezze apprezzabili sono: \n");
+        incertezze
+        fprintf("I modelli con le incertezze accettabili sono: \n");
+        Esito
+        
     otherwise 
         %non fa niente
 end
-
-
-%{
-%VALIDAZIONE%
-
-n_arx = input("Quanti sono i modelli ARX su cui fare la validazione?\n");
-for i = 1 : n_arx
-    modelli = input("Inserisci l'ordine del modello da validare:\n");
-    modelli = num2str(modelli);
-    theta = arx(dati_val,[modelli(i,1),modelli(i,2),modelli(i,3)]);
-    fprintf("Modello ARX(");
-    fprintf("%d", modelli(i,1));
-    fprintf(",")
-    fprintf("%d", modelli(i,2));
-    fprintf(",")
-    fprintf("%d", modelli(i,3));
-    fprintf(") \n\n");
-    Errore_val_arx(theta,dati_val,modelli(i,1),modelli(i,2),modelli(i,3));
-end
-
-n_armax = input("Quanti sono i modelli ARMAX su cui fare la validazione?\n");
-for i = 1 : n_armax
-    modelli = input("Inserisci l'ordine del modello da validare:\n");
-    modelli = num2str(modelli);
-    theta = armax(dati_val,[modelli(i,1),modelli(i,2),modelli(i,3),modelli(i,4)]);
-    fprintf("Modello ARMAX(");
-    fprintf("%d",modelli(i,1));
-    fprintf(",")
-    fprintf("%d",modelli(i,2));
-    fprintf(",")
-    fprintf("%d",modelli(i,3));
-    fprintf(",")
-    fprintf("%d",modelli(i,4));
-    fprintf(") \n\n");
-    Errore_val_armax(theta,dati_val,modelli(i,1),modelli(i,2),modelli(i,3),modelli(i,4));
-end
-
-%}
