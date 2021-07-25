a5e3df47a90137594ecc84db3158fee45a2bb49e
francescacolacrai <64028257+francescacolacrai@users.noreply.github.com>
2021-07-25 18:05:59 +0200

    Test di Anderson

--- test_di _anderson.py
+++ test_di _anderson.py
@@ -5,15 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm as nm
 
-#Importiamo i dati del file 
+#Importiamo i dati del file 'segnale.py' contenente la generazione del segnale fatta in precedenza
 from segnale import gauss
-    
-#generazione segnale per provare la funzione 'Anderson'
-#np.random.seed(1)
-#media = 2
-#var = 2
-#dev = np.sqrt(var)
-#signal = media + dev * np.random.randn(10000)
 
 #Trasposto del segnale per il calcolo della covarianza campionaria
 gauss_t = gauss.transpose()
@@ -26,32 +19,34 @@ tau_max = 30
 
 def Anderson(rumore,tau_max=30,alpha=0.05) :
 
-    #Controllo del passaggio del parametro tau_max
-    if tau_max >= len(gauss) or tau_max <0:
-        print('\n')
-        raise ValueError('*** tau_max deve essere positivo ed minore della lungezza del segnale ***')
-        
-    #Determino il parametro beta: 
-    #forniamo 'alpha/2' perché consideriamo la singola coda (l'area complessiva delle due code è 5)
-    beta = abs(nm.ppf(alpha/2))
-    print ('Il parametro beta vale: \n', round(beta,6)) 
-
     #Controllo livello di significatività alpha
     if alpha <= 0 or alpha >= 1 :
         print('\n')
         raise ValueError('*** Il livello di significatività alpha deve stare in (0,1) ***')
 
+     #Controllo del passaggio del parametro tau_max
+    if tau_max >= len(gauss) or tau_max <0:
+        print('\n')
+        raise ValueError('*** tau_max deve essere positivo ed minore della lungezza del segnale ***')
+       
     #Calcolo una stima della funzione di covarianza campionaria normalizzata
     def Rho(segnale,tau_max) :
+        #Inizializzo gamma
         gamma = np.zeros((tau_max+1),dtype=int)
         segnale_t = segnale.transpose()
-        #gamma_zero = (1/tau_max) * np.dot(segnale[0:tau_max], segnale_t[0:tau_max])
         for t in range(0,tau_max):
             gamma[t] = (1/tau_max) * np.dot(segnale[0:tau_max-t], segnale_t[t:tau_max])
+        #Calcolo di una stima della varianza campionaria normalizzata:
+        #gamma[0] corrisponde ad una stima della varianza campionaria del segnale
         rho = gamma / gamma[0]
         return rho
 
-    print('\nRho vale: \n', Rho(rumore,tau_max))
+    #print('\nRho vale: \n', Rho(rumore,tau_max))
+
+    #Determino il parametro beta: 
+    #forniamo 'alpha/2' perché consideriamo la singola coda (l'area complessiva delle due code è 5)
+    beta = abs(nm.ppf(alpha/2))
+    print ('\n Il parametro beta vale: \n', round(beta,6)) 
 
     #estremo dell'intervallo di confidenza
     estremo = beta/np.sqrt(tau_max)
